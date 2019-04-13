c4d6f7b40b874d14ddc753e86dfda385d1c82f9f
Armen Nikoyan <armen.nikoyan@genesis.ae>
2019-04-13 19:32:06 +0400

    ssh fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,8 +9,10 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/nikoyan-rsa # Allow read access to the private key
 ssh-add .travis/nikoyan-rsa # Add the private key to SSH
 
+ssh -o "StrictHostKeyChecking no" $USER@$ADDRESS #Skip known_hosts promt
+
 git config --global push.default matching
-git remote add deploy ssh://git@$ADDRESS:$PORT$DEPLOY_DIR
+git remote add deploy ssh://$USER@$ADDRESS:$PORT$DEPLOY_DIR
 git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.
